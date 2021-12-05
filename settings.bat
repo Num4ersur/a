9717905b73a600dbce20a3d8e43279fabcd1e02b
Num4ersur <94721285+Num4ersur@users.noreply.github.com>
2021-12-05 20:53:56 +0100

    Add files via upload

---
+++ settings.bat
@@ -0,0 +1,25 @@
+start ms-settings:
+timeout /t 2
+start ms-settings:windowsinsider
+timeout /t 1
+start ms-settings:taskbar
+timeout /t 1
+start ms-settings:accounts
+timeout /t 1
+start ms-settings:about
+timeout /t 1
+start ms-settings:personalization
+timeout /t 1
+start ms-settings:date
+timeout /t 1
+start ms-settings:windowsupdate
+timeout /t 1
+start ms-settings:privacy
+timeout /t 1
+start ms-settings:windowsdefender
+timeout /t 1
+start ms-settings:defaultapps
+timeout /t 1
+start ms-settings:bluetooth
+timeout /t 1
+start ms-settings:connecteddevices
---
+++ uwp.bat
@@ -0,0 +1,4 @@
+start ms-Teams:
+start ms-settings:windowsinsider
+start ms-gamingoverlay:///
+start ms-actioncenter:controlcenter/&suppressAnimations=false&showFooter=true&allowPageNavigation=true 
